06346b388d9a9cab8758491881dcecccd8dbb564
VPF557 <pedrojosepaniagua01@gmail.com>
2023-06-08 18:58:49 +0200

    Creado test comprobando que un objeto se ha añadido correctamente

--- Backend/src/main/java/Plantilla/example/Plantilla/DataHanding.java
+++ Backend/src/main/java/Plantilla/example/Plantilla/DataHanding.java
@@ -28,6 +28,33 @@ public class DataHanding {
         return listaAux;
     }
 
+    public int ComprobarObjeto(Criptomoneda objeto1, String ruta , ArrayList<Criptomoneda> listaAux)
+    {
+        //En esta funcion se añaden los nuevos elementos enviados desde el front a un ArrayList para posteriormente actualizar la BBDD
+        LeerJson reader = new LeerJson();
+        int control = 0;
+
+        for (int i = 0; i < listaAux.size(); i++)
+        {
+            if
+            (       objeto1.getNombre().equals(listaAux.get(i).getNombre()) &&
+                    objeto1.getValor() ==  listaAux.get(i).getValor() &&
+                    objeto1.getFluctuacion() == listaAux.get(i).getFluctuacion() &&
+                    objeto1.getCategoria().equals(listaAux.get(i).getCategoria()) &&
+                    objeto1.getPopularidad().equals(listaAux.get(i).getPopularidad())
+            )
+            {
+                control = 1;
+            }
+            else
+            {
+                control = 0;
+            }
+
+        }
+        return control;
+    }
+
 
 
 }
---
+++ Backend/src/test/java/Plantilla/example/Plantilla/DataHandingTest.java
@@ -0,0 +1,33 @@
+package Plantilla.example.Plantilla;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
+public class DataHandingTest {
+
+    @Test
+    public void anadirObjeto1()
+    {
+        DataHanding data = new DataHanding();
+        ArrayList<Criptomoneda> listaAux= new ArrayList<Criptomoneda>();
+        Criptomoneda pr1 = new Criptomoneda(0,"SuperCoin",129,78.36,"Terciaria","Media");
+        listaAux = data.AnadirObjeto1(pr1,"Inventario.json");
+
+        if(data.ComprobarObjeto(pr1,"Criptomonedas.json",listaAux) == 1)
+        {
+            assertTrue(true);
+        }
+        else
+        {
+            fail("No se ha añadido correctamente");
+        }
+    }
+
+    @Test
+    public void eliminarObjeto()
+    {
+    }
+}
