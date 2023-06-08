1a404d343ec64c3cc88f66f72ca9f1bdfa95ff2b
VPF557 <pedrojosepaniagua01@gmail.com>
2023-06-08 17:56:37 +0200

    Añadido atributo de id a la clase criptomonedas

---
+++ Backend/src/main/java/Plantilla/example/Plantilla/Controller.java
@@ -0,0 +1,42 @@
+package Plantilla.example.Plantilla;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+
+@RestController
+public class Controller {
+
+    @GetMapping("/Cripto")
+    public ArrayList<Criptomoneda> listaObjeto1()
+    {
+        LeerJson reader = new LeerJson();
+        //Leemos el reason e introducimos los elementos en un arraylist
+        ArrayList<Criptomoneda> lista = reader.LeerFicheroJson1("Tweets.json");
+        //Mostramos los elementos leidos
+        return lista;
+    }
+
+    @PostMapping("/CriptoA")
+    public ArrayList<Criptomoneda> create(@RequestBody Criptomoneda objeto1)
+    {
+        DataHanding dataHanding = new DataHanding();
+        ArrayList<Criptomoneda> lista;
+        lista = dataHanding.AnadirObjeto1(objeto1,"Tweets.json");
+        EscribirJson escribirJSON = new EscribirJson();
+        escribirJSON.escribirObjeto1(lista);
+        return lista;
+    }
+
+    @DeleteMapping("/CriptoB{id}")
+    public ArrayList<Criptomoneda> Borrar(@RequestParam int id)
+    {
+        DataHanding dataHanding = new DataHanding();
+        ArrayList<Criptomoneda> lista;
+        lista = dataHanding.EliminarObjeto(id,"Tweets.json");
+        EscribirJson escribirJSON = new EscribirJson();
+        escribirJSON.escribirObjeto1(lista);
+        return lista;
+    }
+
+}
