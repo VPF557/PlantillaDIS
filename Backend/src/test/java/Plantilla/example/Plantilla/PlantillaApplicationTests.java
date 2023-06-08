b3650830437d4940187e67a2de561a16bb02cf34
VPF557 <pedrojosepaniagua01@gmail.com>
2023-06-08 17:38:25 +0200

    Creada estructura basica

---
+++ Backend/src/test/java/Plantilla/example/Plantilla/PlantillaApplicationTests.java
@@ -0,0 +1,13 @@
+package Plantilla.example.Plantilla;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantillaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
